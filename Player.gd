edbe9e69f0a617da5529e8c35bf391db816c6229
Samith Shetty <samithshetty0@gmail.com>
2023-11-03 18:31:10 -0400

    Added basic Player Controller

---
+++ Player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var moveSpeed: float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	move_and_slide()
+
+func _physics_process(delta):
+	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * moveSpeed
+	print(velocity)

f1ecea47fb7ac51b9eb1367b98f9952954d7dd9f
Samith Shetty <samithshetty0@gmail.com>
2023-11-04 10:46:00 -0400

    Implemented Roaming and Tweaked Boomerang Backstab check

--- NPC/NPC.gd
+++ NPC/NPC.gd
@@ -4,6 +4,7 @@ class_name NPC
 var player: CharacterBody2D
 var is_aggro: bool = false
 var chase_direction : Vector2
+var target_location: Vector2
 @export var move_speed: float
 @onready var sprite = $AnimatedSprite2D
 @onready var nav_agent = $NavigationAgent2D
@@ -11,11 +12,19 @@ var chase_direction : Vector2
 func _ready():
 	player = get_node("../Player")
 	print(player)
-
+	set_roam_target()
+	print(is_aggro)
+	
 func _process(delta):
+	print(is_aggro)
 	if is_aggro:
-		chase_direction = (player.position - position).normalized()
-		velocity = chase_direction * move_speed
+		target_location = player.position
+		
+	elif position.distance_squared_to(target_location) < 200:
+		set_roam_target()
+		
+	chase_direction = (target_location - position).normalized()
+	velocity = chase_direction * move_speed
 	update_sprite()
 
 func _physics_process(delta):
@@ -26,11 +35,15 @@ func update_sprite():
 	if velocity:
 		sprite.play("run")
 
-
 func _on_area_2d_body_entered(body):
 	if body.name == "Boomerang":
 		if is_aggro:
-			if abs(rad_to_deg(chase_direction.angle_to(body.velocity))) < 15:
+			if chase_direction.dot(body.velocity) > 0:
 				queue_free()
+			else:
+				pass
 		else:
 			is_aggro = true
+
+func set_roam_target():
+	target_location = position + randi_range(200, 500)*Vector2.from_angle(randf()*2*PI) 
